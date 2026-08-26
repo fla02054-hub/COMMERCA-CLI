6c3e9ea3716127d34d23c389bbb01723597e255d
fla02054-hub <fla02054@gmail.com>
2026-08-27 04:32:32 +0700

    feat: export browser-based Shopee discovery APIs

--- src/product/index.ts
+++ src/product/index.ts
@@ -1,10 +1,21 @@
 export type { Product } from "./types.js";
 export type { ProductDiscovery } from "./discovery.js";
 
-export { LocalProductDiscovery } from "./local-discovery.js";
+export {
+  LocalProductDiscovery,
+  type LocalProductDiscoveryOptions,
+} from "./local-discovery.js";
 
 export type { ProductProvider } from "./providers/index.js";
-export { ShopeeProvider } from "./providers/index.js";
+export {
+  ShopeeProvider,
+  type ShopeeSearchOptions,
+} from "./providers/index.js";
+
+export {
+  readShopeeProductDetail,
+  type ShopeeProductDetail,
+} from "./shopee-detail.js";
 
 export { ProductProviderRegistry } from "./registry.js";
 
