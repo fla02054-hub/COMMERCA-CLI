0284e03553c594a3912ce40fef34958668a3a6c8
fla02054-hub <fla02054@gmail.com>
2026-08-28 23:29:00 +0700

    feat: register Rakatookyang provider

--- src/product/providers/index.ts
+++ src/product/providers/index.ts
@@ -1,3 +1,4 @@
 export type { ProductProvider } from "./provider.js";
 export { ShopeeProvider } from "./shopee.js";
 export { ShopeeBrowserProvider } from "./shopee-browser.js";
+export { RakatookyangProvider } from "./rakatookyang.js";
