5f02d9d328faded0905e7d2f86e116df7fce1094
fla02054-hub <fla02054@gmail.com>
2026-08-30 03:42:37 +0700

    remove product search providers

--- src/product/providers/index.ts
+++
@@ -1,4 +0,0 @@
-export type { ProductProvider } from "./provider.js";
-export { ShopeeProvider } from "./shopee.js";
-export { ShopeeBrowserProvider } from "./shopee-browser.js";
-export { RakatookyangBrowserProvider as RakatookyangProvider } from "./rakatookyang-browser.js";
