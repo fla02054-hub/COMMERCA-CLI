e3aaf1016a7d43d63e99c2204321e873b9ab3b60
fla02054-hub <fla02054@gmail.com>
2026-08-30 03:37:05 +0700

    fix: use reliable RakaTookYang browser provider

--- src/product/providers/index.ts
+++ src/product/providers/index.ts
@@ -1,4 +1,4 @@
 export type { ProductProvider } from "./provider.js";
 export { ShopeeProvider } from "./shopee.js";
 export { ShopeeBrowserProvider } from "./shopee-browser.js";
-export { RakatookyangProvider } from "./rakatookyang.js";
+export { RakatookyangBrowserProvider as RakatookyangProvider } from "./rakatookyang-browser.js";
